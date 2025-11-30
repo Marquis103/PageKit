6a2a7fb56c1d433e508965138c05a5ef68ff52ab
Marquis Dennis <marquis103@gmail.com>
2025-11-30 13:17:38 -0500

    update test name

--- Tests/PageFrameworkTests/PageFrameworkTests.swift
+++ Tests/PageFrameworkTests/PageFrameworkTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import ModuleFramework
+@testable import PageFramework
 
-final class ModuleFrameworkTests: XCTestCase {
+final class PageFrameworkTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
